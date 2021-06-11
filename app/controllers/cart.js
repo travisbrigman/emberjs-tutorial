3118da6ed570bc57636a293901218d512f901319
travisbrigman <travis1000@gmail.com>
2021-06-11 10:57:14 -0500

    through video 10 on tutorial

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,13 +1,29 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class CartController extends Controller {
+  @service('shopping-cart') cart;
   get subtotal() {
-    return this.model.reduce((acc, item) => acc + item.price, 0);
+    return this.cart.itemList.reduce(
+      (acc, item) => acc + item.price * item.count,
+      0
+    );
   }
   get tax() {
     return this.subtotal * 0.09;
   }
   get total() {
     return this.subtotal + this.tax;
+  }
+
+  @action
+  updateItemCount(item, event) {
+    const count = event.target.value;
+    if (count >= 0) {
+      item.count = count;
+    } else {
+      item.count = 0;
+    }
   }
 }
