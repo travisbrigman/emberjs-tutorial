aa405de591fb5e22d86d8a465559575c04f9c5cc
travisbrigman <travis1000@gmail.com>
2021-06-10 21:47:16 -0500

    through video 7 on tutorial

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -4,7 +4,6 @@ export default class CartController extends Controller {
   get subtotal() {
     return this.model.reduce((acc, item) => acc + item.price, 0);
   }
-
   get tax() {
     return this.subtotal * 0.09;
   }
--- app/routes/item.js
+++ app/routes/item.js
@@ -1,8 +1,16 @@
 import Route from '@ember/routing/route';
+import { products } from '../data/products';
 
 export default class ItemRoute extends Route {
   model(params) {
     const { item_id } = params;
-    return item_id;
+    const product = products.find(({ id }) => id === item_id);
+
+    return product;
+  }
+
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    controller.color = model.colors[0].color;
   }
 }
